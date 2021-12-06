b2fc298bef977dbf5f59fc6d123d7caa3ba970b5
kc-hrt <kaseyh@pm.me>
2021-12-06 01:45:23 -0600

    missing files

---
+++ build/precache-manifest.c5da66fdd89df40f28915b10c5197665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052f6f1be227620fc1e6a53a407ee656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdc02feaef375981f3e",
+    "url": "/static/css/main.c060f3db.chunk.css"
+  },
+  {
+    "revision": "6b0729e00421ff5f065f",
+    "url": "/static/js/2.50df07df.chunk.js"
+  },
+  {
+    "revision": "cbdc02feaef375981f3e",
+    "url": "/static/js/main.d5129367.chunk.js"
+  },
+  {
+    "revision": "ec65c1190d40eb81833d",
+    "url": "/static/js/runtime~main.b913a435.js"
+  },
+  {
+    "revision": "412417f47ad0b06d338d0479102375cf",
+    "url": "/static/media/pansey_bg.412417f4.jpg"
+  }
+]);
